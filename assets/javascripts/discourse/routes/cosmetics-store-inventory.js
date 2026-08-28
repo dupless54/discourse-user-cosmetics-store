97d02719e9560e8e1c7323cace3905e6bbe5761a
dupless54 <126967564+dupless54@users.noreply.github.com>
2026-08-28 23:16:14 +0300

    PERF: avoid loading Store catalog on inventory route

--- assets/javascripts/discourse/routes/cosmetics-store-inventory.js
+++ assets/javascripts/discourse/routes/cosmetics-store-inventory.js
@@ -4,16 +4,12 @@ import CosmeticsStoreBaseRoute from "./cosmetics-store-base";
 export default class CosmeticsStoreInventoryRoute extends CosmeticsStoreBaseRoute {
   storeTab = "inventory";
 
-  async model(params = {}) {
-    const [store, inventory] = await Promise.all([
-      super.model(params),
-      ajax("/cosmetics-store/inventory.json"),
-    ]);
+  async model() {
+    const payload = await ajax("/cosmetics-store/inventory.json");
 
     return {
-      ...store,
-      inventory: inventory.inventory,
-      inventory_collections: inventory.collections,
+      ...payload,
+      route_tab: this.storeTab,
     };
   }
 }
