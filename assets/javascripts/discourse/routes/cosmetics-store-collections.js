994a246005af6aac75a794e165bf2f6f719cb287
dupless54 <126967564+dupless54@users.noreply.github.com>
2026-08-28 23:03:33 +0300

    FEATURE: enrich collections with ownership progress

--- assets/javascripts/discourse/routes/cosmetics-store-collections.js
+++ assets/javascripts/discourse/routes/cosmetics-store-collections.js
@@ -1,5 +1,24 @@
+import { ajax } from "discourse/lib/ajax";
 import CosmeticsStoreBaseRoute from "./cosmetics-store-base";
 
 export default class CosmeticsStoreCollectionsRoute extends CosmeticsStoreBaseRoute {
   storeTab = "collections";
+
+  async model(params = {}) {
+    const [store, progress] = await Promise.all([
+      super.model(params),
+      ajax("/cosmetics-store/inventory.json?scope=collections"),
+    ]);
+    const progressBySlug = new Map(
+      (progress.collections || []).map((collection) => [collection.slug, collection])
+    );
+
+    return {
+      ...store,
+      collections: (store.collections || []).map((collection) => ({
+        ...collection,
+        ...(progressBySlug.get(collection.slug) || {}),
+      })),
+    };
+  }
 }
