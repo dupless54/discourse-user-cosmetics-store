a0f488369a7d5579c005a7602d8ddb8693c2b05a
dupless54 <126967564+dupless54@users.noreply.github.com>
2026-08-28 22:10:45 +0300

    FEATURE: add Store inventory route

--- assets/javascripts/discourse/discourse-cosmetics-store-route-map.js
+++ assets/javascripts/discourse/discourse-cosmetics-store-route-map.js
@@ -6,6 +6,7 @@ export default function cosmeticsStoreRoutes() {
   });
   this.route("cosmetics-store-orbs", { path: "/store/orbs" });
   this.route("cosmetics-store-favorites", { path: "/store/favorites" });
+  this.route("cosmetics-store-inventory", { path: "/store/inventory" });
   this.route("cosmetics-store-collections", { path: "/store/collections" });
   this.route("cosmetics-store-collection", {
     path: "/store/collections/:collection_slug",
