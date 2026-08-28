5f61aa3df5c133cc1f2dad3bcb757e9bb8669852
dupless54 <126967564+dupless54@users.noreply.github.com>
2026-08-28 23:37:40 +0300

    FEATURE: add store history sidebar link

--- assets/javascripts/discourse/initializers/add-cosmetics-inventory-to-sidebar.js
+++ assets/javascripts/discourse/initializers/add-cosmetics-inventory-to-sidebar.js
@@ -22,6 +22,16 @@ export default {
           defaultPrefixValue = "image";
         };
       });
+
+      api.addCommunitySectionLink((baseSectionLink) => {
+        return class CosmeticsHistorySectionLink extends baseSectionLink {
+          name = "cosmetics-history";
+          route = "cosmetics-store-history";
+          text = i18n("discourse_cosmetics_store.nav.history");
+          title = i18n("discourse_cosmetics_store.history.subtitle");
+          defaultPrefixValue = "cart-shopping";
+        };
+      });
     });
   },
 };
