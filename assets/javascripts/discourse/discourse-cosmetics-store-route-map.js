2e0cea4793f824ccab36371372fbb1f23e2fcfbb
dupless54 <126967564+dupless54@users.noreply.github.com>
2026-08-29 03:22:53 +0300

    INTEGRATION: route private Store history

--- assets/javascripts/discourse/discourse-cosmetics-store-route-map.js
+++ assets/javascripts/discourse/discourse-cosmetics-store-route-map.js
@@ -10,6 +10,7 @@ export default function cosmeticsStoreRoutes() {
   this.route("cosmetics-store-loadouts", { path: "/store/loadouts" });
   this.route("cosmetics-store-preview", { path: "/store/preview" });
   this.route("cosmetics-store-activity", { path: "/store/activity" });
+  this.route("cosmetics-store-history", { path: "/store/history" });
   this.route("cosmetics-store-collections", { path: "/store/collections" });
   this.route("cosmetics-store-collection", {
     path: "/store/collections/:collection_slug",
