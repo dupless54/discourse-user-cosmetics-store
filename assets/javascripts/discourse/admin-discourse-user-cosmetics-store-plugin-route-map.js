cf3ed9cc643e3b49a49fef83b116625eb6fee316
dupless54 <126967564+dupless54@users.noreply.github.com>
2026-08-31 01:03:04 +0300

    REFACTOR: add product new and edit admin routes

--- assets/javascripts/discourse/admin-discourse-user-cosmetics-store-plugin-route-map.js
+++ assets/javascripts/discourse/admin-discourse-user-cosmetics-store-plugin-route-map.js
@@ -5,7 +5,10 @@ export default {
 
   map() {
     this.route("cosmetics-store-catalog", { path: "catalog" });
-    this.route("cosmetics-store-products", { path: "products" });
+    this.route("cosmetics-store-products", { path: "products" }, function () {
+      this.route("new");
+      this.route("edit", { path: "/:id/edit" });
+    });
     this.route("cosmetics-store-missions", { path: "missions" });
     this.route("cosmetics-store-payments", { path: "payments" });
     this.route("cosmetics-store-wallets", { path: "wallets" });
